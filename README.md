844f4dc7fea18782e4512d999b593405ab3c5d3e
bangun <morpheusgun@outlook.com>
2020-05-24 17:29:32 +0700

    change from flag to os args to support beautifying multiple files, use goroutine for beautifying process, add test files, update readme, add license copy into source code

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 A simple tool to beautify your JSON file
 
 ## Usage
-`./json-beautify -f=yourfilename`
+`$ ./json-beautify yourfile1.json yourfile2.json yourfile3.json`
 This will beautify and overwrite your JSON file.
+If there's any error during beautifying the JSON file, the error message will be printed.
--- main.go
+++ main.go
@@ -1,52 +1,90 @@
 package main
 
+// MIT License
+//
+// Copyright (c) 2018 Yuwono Bangun Nagoro
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 import (
 	"encoding/json"
-	"flag"
 	"fmt"
 	"io/ioutil"
 	"os"
+	"sync"
 )
+
+var wg sync.WaitGroup
 
 // aimed to beautify your wrecked JSON
 // to make it beauty like your crush and your ex
 
 func main() {
-	fileFlag := flag.String("f", "", "Define JSON file path that you want to beautify")
-	flag.Parse()
-	rawJsonFile, err := os.Open(*fileFlag)
-	if err != nil {
-		fmt.Println("😢 Error on opening file :", err.Error())
+	args := os.Args
+
+	if len(args) <= 1 {
+		fmt.Println("😠 JSON files must be defined!")
 		return
 	}
-	defer rawJsonFile.Close()
-	rawJsonBody, err := ioutil.ReadAll(rawJsonFile)
-	if err != nil {
-		fmt.Println("😢 Error on reading file :", err.Error())
-		return
-	}
-	jsonData, err := validateJSON(string(rawJsonBody))
-	if err != nil {
-		fmt.Println("😢 Your JSON is invalid", err.Error())
-		return
+	fileNames := args[1:]
+
+	for _, fn := range fileNames {
+		wg.Add(1)
+		go func(fileName string) {
+			defer wg.Done()
+			rawJsonFile, err := os.Open(fileName)
+			if err != nil {
+				fmt.Printf("😢 Error on opening file %s\n%s\n", fileName, err.Error())
+				return
+			}
+			defer rawJsonFile.Close()
+			rawJsonBody, err := ioutil.ReadAll(rawJsonFile)
+			if err != nil {
+				fmt.Printf("😢 Error on reading file %s\n%s\n", fileName, err.Error())
+				return
+			}
+			jsonData, err := validateJSON(string(rawJsonBody))
+			if err != nil {
+				fmt.Printf("😢 Your JSON on file %s is invalid\n%s\n", fileName, err.Error())
+				return
+			}
+
+			rawJsonData, err := json.MarshalIndent(jsonData, "", "\t")
+			if err != nil {
+				fmt.Printf("😢 Error occured while beautifying your JSON on file %s\n%s\n", fileName, err.Error())
+				return
+			}
+
+			beautifiedPath := fmt.Sprintf("%s", fileName)
+			beautifiedFile, err := os.Create(beautifiedPath)
+			if err != nil {
+				fmt.Printf("😢 Error occured while creating new file %s\n%s\n", fileName, err.Error())
+				return
+			}
+			beautifiedFile.Write(rawJsonData)
+
+			fmt.Printf("😉 Done beautifying your JSON on file %s\n", fileName)
+			defer beautifiedFile.Close()
+		}(fn)
 	}
 
-	rawJsonData, err := json.MarshalIndent(jsonData, "", "\t")
-	if err != nil {
-		fmt.Println("😢 Error occured while beautifying your JSON", err.Error())
-		return
-	}
-
-	beautifiedPath := fmt.Sprintf("%s", *fileFlag)
-	beautifiedFile, err := os.Create(beautifiedPath)
-	if err != nil {
-		fmt.Println("😢 Error occured while creating new file", err.Error())
-		return
-	}
-	beautifiedFile.Write(rawJsonData)
-
-	fmt.Println("😉 Done beautifying your JSON")
-	defer beautifiedFile.Close()
+	wg.Wait()
 }
 func validateJSON(body string) (map[string]interface{}, error) {
 	var temporaryJSONMap map[string]interface{}
